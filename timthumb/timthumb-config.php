163c028cddeba5fe0d641ba39e688f93ac353345
LoeiFy <loeify@gmail.com>
2015-05-16 20:17:18 +0800

    add timthumb

---
+++ timthumb/timthumb-config.php
@@ -0,0 +1,17 @@
+<?php
+
+define ('LIMIT_ON', TRUE);
+
+define ('ALLOW_EXTERNAL', FALSE);
+
+define ('BROWSER_CACHE_MAX_AGE', 86400000);
+
+define ('DEFAULT_WIDTH', 680);
+
+define ('DEFAULT_HEIGHT', 440);
+
+define ('FILE_CACHE_DIRECTORY', './what');
+
+define ('DISPLAY_ERROR_MESSAGES', FALSE);
+
+?>
