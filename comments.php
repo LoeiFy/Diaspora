b38f3524042eb3bde0489e267261708884a3251a
LoeiFy <LoeiFy@gmail.com>
2017-04-05 15:02:08 +0800

    comments nav

--- comments.php
+++ comments.php
@@ -1,25 +1,26 @@
 <div id="comments" class="comments-area">
 
-	<?php if ( have_comments() ) : ?>
-		<h2>评论列表</h2>
-		<ol class="comment-list">
-			<?php
-				wp_list_comments( array(
-					'style'       => 'ol',
-					'short_ping'  => true,
-					'avatar_size' => 40,
-					'per_page'    => 9999,
-				) );
-			?>
-		</ol>
+    <?php if ( have_comments() ) : ?>
+        <h2>评论列表</h2>
+        <ol class="comment-list">
+            <?php
+                wp_list_comments( array(
+                    'style'       => 'ol',
+                    'short_ping'  => true,
+                    'avatar_size' => 40,
+                ) );
+            ?>
+        </ol>
 
-	<?php endif; ?>
+        <?php the_comments_navigation(); ?>
 
-	<?php
-		comment_form( array(
-			'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
-			'title_reply_after'  => '</h2>',
-		) );
-	?>
+    <?php endif; ?>
+
+    <?php
+        comment_form( array(
+            'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+            'title_reply_after'  => '</h2>',
+        ) );
+    ?>
 
 </div>
